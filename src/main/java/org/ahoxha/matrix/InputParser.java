fb8f5ff550e3776a7ea02e28dccfd6adf32ec13c
Hoxha <armend.hoxha@rabobank.nl>
2020-05-30 02:03:13 +0200

    Added test cases for InputParser.

--- src/main/java/org/ahoxha/matrix/InputParser.java
+++ src/main/java/org/ahoxha/matrix/InputParser.java
@@ -1,29 +1,31 @@
 package org.ahoxha.matrix;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import javax.swing.*;
-
 public final class InputParser {
 
-	private InputParser() {}
+    private InputParser() {
+    }
 
-	public static int[] parse(String s) {
-		List<String> vec = new ArrayList<>();
-		StringTokenizer t = new StringTokenizer(s, ",-");
-		int i = 0;
-		while (t.hasMoreTokens()) {
-			i++;
-			vec.add(t.nextToken());
-		}
+    public static int[] parse(String s) {
+        if(s == null || s.isBlank()) {
+            throw new IllegalArgumentException();
+        }
 
-		int[] rez = new int[i];
-		for (int j = 0; j < rez.length; j++) {
-			rez[j] = Integer.parseInt(vec.get(j));
-		}
-		return rez;
-	}
+        List<String> vec = new ArrayList<>();
+        StringTokenizer t = new StringTokenizer(s.replace(" ", ""), ",-");
+        int i = 0;
+        while (t.hasMoreTokens()) {
+            i++;
+            vec.add(t.nextToken());
+        }
+
+        int[] rez = new int[i];
+        for (int j = 0; j < rez.length; j++) {
+            rez[j] = Integer.parseInt(vec.get(j));
+        }
+        return rez;
+    }
 }
